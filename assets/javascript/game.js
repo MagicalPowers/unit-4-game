cc59bd398885935a8c17ac66e7c120d751555783
SuperPowers <53920220+MagicalPowers@users.noreply.github.com>
2019-10-24 08:03:37 -0400

    fixed a bug in the css, trying to complete the functionality of the js.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,11 @@
 $(document).ready(function () {
     var global1 = 0;
-    var global2 = 0;
-    var global3 = 0;
+   // var global2 = (Math.floor(Math.random() * 101 + 19)};
+
+   //i need a random integer between 19 and 120
+    var global3 = function randomIntFromInterval(19, 120) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+      };
     var global4 = 0;
     // ##### Option 1 Game design notes
     // * The random number shown at the start of the game should be between 19 - 120.
@@ -13,26 +17,38 @@ $(document).ready(function () {
     var global8 = "";
     var global9 = "";
     var global10 = "";
-//how do i establish the initial random number shown at the start?
+    //how do i establish the initial random number shown at the start?
+    //somethinng like this:
+
+   // var targetNumber = ;
+
+    $("#trashtoguess").text(global3);
+    console.log(global3);
+
+
+    //i think that will work
+
+
+
 
     //here i want to assign an attribute to the images of cyrstals, something about a random hidden value between one and twelve.
     function function1() {
         //imageCrystal.attr("data-crystalvalue", numberOptions[i]);
         //$("img").attr("width", "500");
         //$(selector).attr(attribute, function (index, currentvalue));
-        document.getElementById("trashcrystal1").setAttribute("trashvalue", (Math.floor(Math.random() * 10)+2));
+        document.getElementById("trashcrystal1").setAttribute("trashvalue", (Math.floor(Math.random() * 10) + 2));
 
     };
     function function2() {
-        document.getElementById("trashcrystal2").setAttribute("trashvalue", (Math.floor(Math.random() * 10)+2));
+        document.getElementById("trashcrystal2").setAttribute("trashvalue", (Math.floor(Math.random() * 10) + 2));
 
     };
     function function3() {
-        document.getElementById("trashcrystal3").setAttribute("trashvalue", (Math.floor(Math.random() * 10)+2));
+        document.getElementById("trashcrystal3").setAttribute("trashvalue", (Math.floor(Math.random() * 10) + 2));
 
     };
     function function4() {
-        document.getElementById("trashcrystal4").setAttribute("trashvalue", (Math.floor(Math.random() * 10)+2));
+        document.getElementById("trashcrystal4").setAttribute("trashvalue", (Math.floor(Math.random() * 10) + 2));
 
     };
     function function5() {
@@ -72,7 +88,7 @@ $(document).ready(function () {
     //it will give an alert
     //this is calledjQuery, it does not belong to me.
     //an onclickevent that should give a useless alert and establish a local variable
-    $(".stolenjunk").on("click", function () {
+    $(".col-3").on("click", function () {
         alert("did you leave your stove on?");
         var trashvalue
 
